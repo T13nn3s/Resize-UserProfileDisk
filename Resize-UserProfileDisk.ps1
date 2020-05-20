0d4d7941c0e56d97a3d5d8ee0c990352bc73b732
Martien van Dijk <madij@MBP-van-Martien.fritz.box>
2020-05-20 20:32:57 +0200

    Resolves #2

--- Resize-UserProfileDisk.ps1
+++ Resize-UserProfileDisk.ps1
@@ -291,7 +291,8 @@ function Resize-UserProfileDisk {
 
             #startregion Check if file is locked
             if ((Test-FileIsLocked -Path $vhdx -ErrorAction SilentlyContinue) -eq $false) {
-                Add-Content diskpart_script.txt "select vdisk file=$vhdx"
+                $fileString = 'select vdisk file="' + $UPD + '"' 
+                Add-Content -Path diskpart_script.txt $fileString
                 Add-Content diskpart_script.txt "attach vdisk readonly"
                 Add-Content diskpart_script.txt "compact vdisk"
                 Write-Log -severity Information -message "$vhdx is accessible."
